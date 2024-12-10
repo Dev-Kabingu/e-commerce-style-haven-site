1914eb7a6c77f5425d2f9eaac471d03a4a162f7f
dev-kabingu <missmercykabingu@gmail.com>
2024-12-10 08:33:29 +0300

    added a login and signup pages

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// const heroBtn = document.getElementById("heroBtn");
+//  heroBtn.addEventListener("click", function(){
+//     window.location.href ="login.html";
+//  });
+
+ document.getElementById("heroBtn").onclick = function(){
+    window.location.href = "login.html";
+}
